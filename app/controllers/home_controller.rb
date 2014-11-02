65ab379b94fc134b56c950e1db7e75f20d147288
Emmanuel Turlay <bluesmanu@hotmail.com>
2014-11-02 15:30:15 -0800

    fix to_i

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     params[:nweeks] = nil if params[:nweeks].try(:strip).try(:blank?)
     params[:nweeks] = all_weeks if params[:all].to_i == 1
-    @nweeks = params[:nweeks] || CohortBuilder::DEFAULT_BACK_WEEKS
+    @nweeks = params[:nweeks].try(:to_i) || CohortBuilder::DEFAULT_BACK_WEEKS
     # to_i on nil is 0, we don't want that, we want nil
     @cohorts = CohortBuilder.build_weekly_cohorts(@nweeks)
   end
